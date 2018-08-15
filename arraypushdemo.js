351e5230d04a9fc9b95259b62ba539b580fbc24d
jadenhasemabry <42406133+jadenhasemabry@users.noreply.github.com>
2018-08-15 12:06:20 -1000

    Add files via upload

---
+++ arraypushdemo.js
@@ -0,0 +1,10 @@
+var firstName = prompt("what is your first name?");
+var lastName = prompt("what is your last name?");
+
+var array1 = [];
+var array2 = [];
+
+array1.push(firstName);
+array2.push(lastName);
+
+window.alert(array1 + " " + array2);
